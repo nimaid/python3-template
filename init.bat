4401c8aa6aadc51e3efc093d54d598b4dba9fc3a
Ella Jameson <ellagjameson@gmail.com>
2023-11-17 00:36:54 -0700

    Added test init bat file

---
+++ init.bat
@@ -0,0 +1,36 @@
+@echo off
+
+REM Run this file to initialize the project
+
+set ORIGDIR="%CD%"
+set FILEDIR="%~dp0"
+
+set INSTALLCONDA="install_conda_windows.bat"
+set SELFNAME="init.bat"
+
+cd %FILEDIR%
+
+REM Install conda
+call cmd /c %INSTALLCONDA%
+if errorlevel 1 goto ERROR
+
+REM Run init.py
+call conda activate base
+call python init.py
+
+REM Install the current conda environment
+call conda env create -f environment.yml
+
+REM Delete the init files
+del /f /q %INSTALLCONDA% 1>nul 2>&1
+del /f /q %SELFNAME% 1>nul 2>&1
+
+cd %ORIGDIR%
+
+:ERROR
+echo Project initialization failed!
+exit /B 1
+
+:DONE
+echo Project initialization done!
+exit /B 0
